f8a0848d69134768dd2bc89757de8d7e3239a317
Gerald <gera2ld@163.com>
2017-04-04 10:06:46 +0800

    chore: add deploy script

---
+++ web/build/deploy.sh
@@ -0,0 +1,9 @@
+set -e
+DIST_PKG=dist.tar.gz
+BASE_DIR='~/Git/web-commander'
+DATA_DIR=$BASE_DIR/data
+DIST_DIR=$BASE_DIR/dist
+
+tar -zcf $DIST_PKG -C dist .
+scp $DIST_PKG gerald:$DATA_DIR
+ssh gerald "mkdir -p $DIST_DIR && tar -zxf $DATA_DIR/$DIST_PKG -C $DIST_DIR"
